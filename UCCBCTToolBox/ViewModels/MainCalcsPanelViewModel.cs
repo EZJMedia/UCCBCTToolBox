efc2ec53e3f1aaa23707f4a5a656b7c9ca78d8d8
ezjmedia <dan@ezjmedia.com>
2025-12-09 23:01:25 -0800

    Fix HF to use signed FA/FAS (Orthospinology)

--- UCCBCTToolBox/ViewModels/MainCalcsPanelViewModel.cs
+++ UCCBCTToolBox/ViewModels/MainCalcsPanelViewModel.cs
@@ -47,6 +47,7 @@ namespace UCCBCTToolBox.ViewModels
             get => _translations;
             set => SetProperty(ref _translations, value);
         }
+
         public int SelectedTranslation
         {
             get => _selectedTranslation;
@@ -61,11 +62,13 @@ namespace UCCBCTToolBox.ViewModels
                 RaisePropertyChanged(nameof(HaxLabel));
             }
         }
+
         public string FACLabel => _categorizedTranslations[Translations[SelectedTranslation]][0];
         public string FASLabel => _categorizedTranslations[Translations[SelectedTranslation]][1];
         public string FALabel => _categorizedTranslations[Translations[SelectedTranslation]][2];
         public string HACLabel => _categorizedTranslations[Translations[SelectedTranslation]][3];
         public string HaxLabel => _categorizedTranslations[Translations[SelectedTranslation]][4];
+
         public static double CValue { get; set; }
         public static double AValue { get; set; }
         public static double HTValue { get; set; }
@@ -91,9 +94,10 @@ namespace UCCBCTToolBox.ViewModels
 
             _translations = new() { "EPIC", "Advanced Orthogonal", "Orthospinology", "NUCCA" };
             List<string> t0 = new() { "FAC", "FAS", "FA", "HAC", "Hax" },
-            t1 = new() { "ACD", "CS", "AFP", "AHRy", "AXSP" },
-            t2 = new() { "Laterality", "Lower Angle", "APL", "C1 Rotation", "AxSP" },
-            t3 = new() { "Laterality", "Lower Angle", "APL", "C1 rotation", "AXSP" };
+                         t1 = new() { "ACD", "CS", "AFP", "AHRy", "AXSP" },
+                         t2 = new() { "Laterality", "Lower Angle", "APL", "C1 Rotation", "AxSP" },
+                         t3 = new() { "Laterality", "Lower Angle", "APL", "C1 rotation", "AXSP" };
+
             _categorizedTranslations = new()
             {
                 { Translations[0], t0 },
@@ -107,131 +111,201 @@ namespace UCCBCTToolBox.ViewModels
         #region Public Properties
         public double HeadTiltInput
         {
-            get { return _headTiltInput; }
-            set { SetProperty(ref _headTiltInput, value); RaisePropertyChanged(nameof(FACCalc)); }
+            get => _headTiltInput;
+            set
+            {
+                SetProperty(ref _headTiltInput, value);
+                RaisePropertyChanged(nameof(FACCalc));
+            }
         }
+
         public bool HTModeR
         {
-            get { return _hTModeR; }
-            set 
+            get => _hTModeR;
+            set
             {
                 SetProperty(ref _hTModeR, value);
                 RaisePropertyChanged(nameof(FACCalc));
             }
         }
+
         public bool HTModeL
         {
-            get { return _hTModeL; }
-            set { SetProperty(ref _hTModeL, value); RaisePropertyChanged(nameof(FACCalc)); }
+            get => _hTModeL;
+            set
+            {
+                SetProperty(ref _hTModeL, value);
+                RaisePropertyChanged(nameof(FACCalc));
+            }
         }
+
         public double SLMInput
         {
-            get { return _sLMInput; }
-            set { SetProperty(ref _sLMInput, value); RaisePropertyChanged(nameof(FACCalc)); }
+            get => _sLMInput;
+            set
+            {
+                SetProperty(ref _sLMInput, value);
+                RaisePropertyChanged(nameof(FACCalc));
+            }
         }
+
         public bool SLMModeR
         {
-            get { return _sLMModeR; }
+            get => _sLMModeR;
             set
             {
                 SetProperty(ref _sLMModeR, value);
                 RaisePropertyChanged(nameof(FACCalc));
             }
         }
+
         public bool SLMModeL
         {
-            get { return _sLMModeL; }
-            set { SetProperty(ref _sLMModeL, value); RaisePropertyChanged(nameof(FACCalc)); }
+            get => _sLMModeL;
+            set
+            {
+                SetProperty(ref _sLMModeL, value);
+                RaisePropertyChanged(nameof(FACCalc));
+            }
         }
+
         public double ILMInput
         {
-            get { return _iLMInput; }
-            set { SetProperty(ref _iLMInput, value); RaisePropertyChanged(nameof(FASCalc)); RaisePropertyChanged(nameof(FACalc)); }
+            get => _iLMInput;
+            set
+            {
+                SetProperty(ref _iLMInput, value);
+                RaisePropertyChanged(nameof(FASCalc));
+                RaisePropertyChanged(nameof(FACalc));
+            }
         }
+
         public bool ILMModeR
         {
-            get { return _iLMModeR; }
-            set { SetProperty(ref _iLMModeR, value); RaisePropertyChanged(nameof(FASCalc)); RaisePropertyChanged(nameof(FACalc)); }
+            get => _iLMModeR;
+            set
+            {
+                SetProperty(ref _iLMModeR, value);
+                RaisePropertyChanged(nameof(FASCalc));
+                RaisePropertyChanged(nameof(FACalc));
+            }
         }
+
         public bool ILMModeL
         {
-            get { return _iLMModeL; }
-            set { SetProperty(ref _iLMModeL, value); RaisePropertyChanged(nameof(FASCalc)); RaisePropertyChanged(nameof(FACalc)); }
+            get => _iLMModeL;
+            set
+            {
+                SetProperty(ref _iLMModeL, value);
+                RaisePropertyChanged(nameof(FASCalc));
+                RaisePropertyChanged(nameof(FACalc));
+            }
         }
+
         public double CSTInput
         {
-            get { return _cSTInput; }
-            set { SetProperty(ref _cSTInput, value); RaisePropertyChanged(nameof(FASCalc)); }
+            get => _cSTInput;
+            set
+            {
+                SetProperty(ref _cSTInput, value);
+                RaisePropertyChanged(nameof(FASCalc));
+            }
         }
+
         public bool CSTModeR
         {
-            get { return _cSTModeR; }
-            set { SetProperty(ref _cSTModeR, value); RaisePropertyChanged(nameof(FASCalc)); RaisePropertyChanged(nameof(ARText)); }
+            get => _cSTModeR;
+            set
+            {
+                SetProperty(ref _cSTModeR, value);
+                RaisePropertyChanged(nameof(FASCalc));
+                RaisePropertyChanged(nameof(ARText));
+            }
         }
+
         public bool CSTModeL
         {
-            get { return _cSTModeL; }
-            set { SetProperty(ref _cSTModeL, value); RaisePropertyChanged(nameof(FASCalc)); RaisePropertyChanged(nameof(ARText)); }
+            get => _cSTModeL;
+            set
+            {
+                SetProperty(ref _cSTModeL, value);
+                RaisePropertyChanged(nameof(FASCalc));
+                RaisePropertyChanged(nameof(ARText));
+            }
         }
+
         public double AAInput
         {
-            get { return _aAInput; }
-            set { SetProperty(ref _aAInput, value); RaisePropertyChanged(nameof(HACCalc)); }
+            get => _aAInput;
+            set
+            {
+                SetProperty(ref _aAInput, value);
+                RaisePropertyChanged(nameof(HACCalc));
+            }
         }
+
         public bool AAModeR
         {
-            get { return _aAModeR; }
+            get => _aAModeR;
             set
             {
                 SetProperty(ref _aAModeR, value);
                 RaisePropertyChanged(nameof(HACCalc));
             }
         }
+
         public bool AAModeL
         {
-            get { return _aAModeL; }
+            get => _aAModeL;
             set
             {
                 SetProperty(ref _aAModeL, value);
                 RaisePropertyChanged(nameof(HACCalc));
             }
         }
+
         public double C2Input
         {
-            get { return _c2Input; }
+            get => _c2Input;
             set
             {
                 SetProperty(ref _c2Input, value);
                 RaisePropertyChanged(nameof(HAxCalc));
             }
         }
+
         public bool C2ModeR
         {
-            get 
-            {
-                return _c2ModeR;
-            }
+            get => _c2ModeR;
             set
             {
                 SetProperty(ref _c2ModeR, value);
                 RaisePropertyChanged(nameof(HAxCalc));
             }
         }
+
         public bool C2ModeL
         {
-            get { return _c2ModeL; }
-            set { SetProperty(ref _c2ModeL, value); RaisePropertyChanged(nameof(HAxCalc)); }
+            get => _c2ModeL;
+            set
+            {
+                SetProperty(ref _c2ModeL, value);
+                RaisePropertyChanged(nameof(HAxCalc));
+            }
         }
+
         public double CInput
         {
-            get { return _cInput; }
-            set { SetProperty(ref _cInput, value); }
+            get => _cInput;
+            set => SetProperty(ref _cInput, value);
         }
+
         public double AInput
         {
-            get { return _aInput; }
-            set { SetProperty(ref _aInput, value); }
+            get => _aInput;
+            set => SetProperty(ref _aInput, value);
         }
+
         public double FACCalc
         {
             get
@@ -246,7 +320,11 @@ namespace UCCBCTToolBox.ViewModels
                 if (SLMModeL)
                     sum -= SLMInput;
 
-                SetConditionalStrings(new bool[] { sum >= 0.0, sum < 0.0 }, new string[] { "R", "L" }, ref _fACText);
+                SetConditionalStrings(
+                    new bool[] { sum >= 0.0, sum < 0.0 },
+                    new string[] { "R", "L" },
+                    ref _fACText);
+
                 RaisePropertyChanged(nameof(FACText));
                 RaisePropertyChanged(nameof(FACalc));
                 RaisePropertyChanged(nameof(HACCalc));
@@ -254,6 +332,7 @@ namespace UCCBCTToolBox.ViewModels
                 return Math.Round(Math.Abs(sum), 2);
             }
         }
+
         public double FASCalc
         {
             get
@@ -268,12 +347,17 @@ namespace UCCBCTToolBox.ViewModels
                 if (CSTModeR)
                     sum -= CSTInput;
 
-                SetConditionalStrings(new bool[] { sum >= 0.0, sum < 0.0 }, new string[] { "R", "L" }, ref _fASText);
+                SetConditionalStrings(
+                    new bool[] { sum >= 0.0, sum < 0.0 },
+                    new string[] { "R", "L" },
+                    ref _fASText);
+
                 RaisePropertyChanged(nameof(FASText));
 
                 return Math.Round(Math.Abs(sum), 2);
             }
         }
+
         public double FACalc
         {
             get
@@ -292,7 +376,11 @@ namespace UCCBCTToolBox.ViewModels
                 if (FACText != null && FACText == "L")
                     sum *= -1.0;
 
-                SetConditionalStrings(new bool[] { sum >= 0.0, sum < 0.0 }, new string[] { "+", "-" }, ref _fAText);
+                SetConditionalStrings(
+                    new bool[] { sum >= 0.0, sum < 0.0 },
+                    new string[] { "+", "-" },
+                    ref _fAText);
+
                 RaisePropertyChanged(nameof(FAText));
 
                 return Math.Round(Math.Abs(sum), 2);
@@ -303,8 +391,15 @@ namespace UCCBCTToolBox.ViewModels
         {
             get
             {
-                SetConditionalStrings(new bool[] { (FACText == "R" && AAModeR) || (FACText == "L" && AAModeL), (FACText == "L" && AAModeR) || (FACText == "R" && AAModeL) },
-                    new string[] { "A", "P" }, ref _hACText);
+                SetConditionalStrings(
+                    new bool[]
+                    {
+                        (FACText == "R" && AAModeR) || (FACText == "L" && AAModeL),
+                        (FACText == "L" && AAModeR) || (FACText == "R" && AAModeL)
+                    },
+                    new string[] { "A", "P" },
+                    ref _hACText);
+
                 RaisePropertyChanged(nameof(HACText));
 
                 return Math.Round(AAInput, 2);
@@ -315,7 +410,11 @@ namespace UCCBCTToolBox.ViewModels
         {
             get
             {
-                SetConditionalStrings(new bool[] { C2ModeR, C2ModeL }, new string[] { "R", "L" }, ref _hAxText);
+                SetConditionalStrings(
+                    new bool[] { C2ModeR, C2ModeL },
+                    new string[] { "R", "L" },
+                    ref _hAxText);
+
                 RaisePropertyChanged(nameof(HAxText));
 
                 return Math.Round(C2Input, 2);
@@ -323,34 +422,44 @@ namespace UCCBCTToolBox.ViewModels
         }
 
         // Orthospinology public properties
-        public double HF => (FACalc * 0.4) + ((AInput - CInput) / 2.0) + (FACText != FASText ? (Math.Abs(FACCalc - FASCalc) * 0.25) : ((FACCalc - FASCalc) * 0.25));
+        public double HF
+        {
+            get
+            {
+                // Get signed versions of FA (FACalc) and FAS
+                double faSigned = GetSignedFA();
+                double fasSigned = GetSignedFAS();
 
-        public double RA => Math.Round(Math.Asin(Math.Sqrt(Math.Pow(HF, 2.0) + Math.Pow(HACCalc, 2.0)) / 20.0) * (180.0 / Math.PI), 2);
+                // First term: FA * 0.4, but now FA can be positive or negative
+                double firstTerm = faSigned * 0.4;
 
-        public string FACText
-        {
-            get { return _fACText; }
+                // Second term: same as before
+                double secondTerm = (AInput - CInput) / 2.0;
+
+                // Third term: same structure as before, but using signed values
+                double thirdTerm = FACText != FASText
+                    ? Math.Abs(faSigned - fasSigned) * 0.25
+                    : (faSigned - fasSigned) * 0.25;
+
+                return firstTerm + secondTerm + thirdTerm;
+            }
         }
-        public string FASText
-        {
-            get { return _fASText; }
-        }
-        public string FAText
-        {
-            get { return _fAText; }
-        }
-        public string HACText
-        {
-            get { return _hACText; }
-        }
-        public string HAxText
-        {
-            get { return _hAxText; }
-        }
-        public string ARText
-        {
-            get { return CSTModeR ? "R" : "L"; }
-        }
+
+        public double RA => Math.Round(
+            Math.Asin(Math.Sqrt(Math.Pow(HF, 2.0) + Math.Pow(HACCalc, 2.0)) / 20.0) * (180.0 / Math.PI),
+            2);
+
+        public string FACText => _fACText;
+
+        public string FASText => _fASText;
+
+        public string FAText => _fAText;
+
+        public string HACText => _hACText;
+
+        public string HAxText => _hAxText;
+
+        public string ARText => CSTModeR ? "R" : "L";
         #endregion
 
         #region Public Methods
@@ -382,10 +491,12 @@ namespace UCCBCTToolBox.ViewModels
             AValue = 0;
             CValue = 0;
         }
+
         public void Autoload()
         {
             Tool current = Global.CurrentTool;
             Debug.WriteLine("Autoload called, active tool is " + current.Name);
+
             if (current.Name == ThreePointCircle.ToolName)
             {
                 if (ThreePointCircle.ToolType == ThreePointCircle.CTool)
@@ -397,6 +508,7 @@ namespace UCCBCTToolBox.ViewModels
                     AInput = AValue;
                 }
             }
+
             if (current.Name == HAVATool.ToolName)
             {
                 HeadTiltInput = Math.Round(Math.Abs(HTValue), 2);
@@ -407,6 +519,48 @@ namespace UCCBCTToolBox.ViewModels
         #endregion
 
         #region Private Methods
+
+        // Returns FA (FACalc) with its sign preserved, based on ILM/SLM and FACText
+        private double GetSignedFA()
+        {
+            double sum = 0;
+
+            // Same structure as FACalc, but WITHOUT Math.Abs and WITHOUT rounding
+            if (ILMModeL)
+                sum -= ILMInput;
+            if (ILMModeR)
+                sum += ILMInput;
+            if (SLMModeR)
+                sum += SLMInput;
+            if (SLMModeL)
+                sum -= SLMInput;
+
+            sum /= 2.0;
+
+            // Apply the FACText-based side convention (L makes it negative)
+            if (!string.IsNullOrEmpty(FACText) && FACText == "L")
+                sum *= -1.0;
+
+            return sum;
+        }
+
+        // Returns FAS with its sign preserved (same sum logic as FASCalc, but no Math.Abs)
+        private double GetSignedFAS()
+        {
+            double sum = 0;
+
+            if (ILMModeL)
+                sum += ILMInput;
+            if (ILMModeR)
+                sum -= ILMInput;
+            if (CSTModeL)
+                sum += CSTInput;
+            if (CSTModeR)
+                sum -= CSTInput;
+
+            return sum;
+        }
+
         private void SetConditionalStrings(bool[] conditions, string[] correspondingValues, ref string storage)
         {
             for (int i = 0; i < conditions.Length; i++)
@@ -424,6 +578,7 @@ namespace UCCBCTToolBox.ViewModels
             RaisePropertyChanged(nameof(HF));
             RaisePropertyChanged(nameof(RA));
         }
+
         #endregion
     }
 }
